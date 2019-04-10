a4a63370ea079e6130031ccfda4164303e929de7
Francis Mendez <fmendez884@gmail.com>
2019-04-10 15:58:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,28 +1,28 @@
-# # Write your code here.
+# Write your code here.
 
-# def badge_maker(name)
-#   return "Hello, my name is #{name}."
-# end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
 
-# def batch_badge_creator(array)
-#   badges = []
-#   i = 0
-#   while ( i < array.length)
-#     badges.push("Hello, my name is #{array[i]}.")
-#     i += 1
-#   end
-#   return badges
-# end
+def batch_badge_creator(array)
+  badges = []
+  i = 0
+  while ( i < array.length)
+    badges.push("Hello, my name is #{array[i]}.")
+    i += 1
+  end
+  return badges
+end
 
-# def assign_rooms(array)
-#   rooms = []
-#     i = 0
-#     while ( i < array.length)
-#       rooms.push("Hello, #{array[i]}! You'll be assigned to room #{i+1}!")
-#       i += 1
-#     end
-#     return rooms
-# end
+def assign_rooms(array)
+  rooms = []
+    i = 0
+    while ( i < array.length)
+      rooms.push("Hello, #{array[i]}! You'll be assigned to room #{i+1}!")
+      i += 1
+    end
+    return rooms
+end
 
 # def printer(batch_badge_creator(array), assign_rooms(array))
 #   puts batch_badge_creator(array)
